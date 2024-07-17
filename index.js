67d02a8a31e75afc6b77b120170049201705f2c4
Ravicha-Kit <ravicha.p@mologtech.com>
2024-07-17 11:44:40 +0700

    fix(client): update active module
    - Remove PSO, Packed, Crosscheck
    - Add AR, BASE, GRR, Inventory Movement
    - Reorder declare client class
    - Update Example on index.js

--- index.js
+++ index.js
@@ -20,6 +20,112 @@ async function main() {
 
     /*
 
+    // Example Advance Return
+    const createAR = await client.AR.create({
+      "Rec1 No": "RET0000000001",
+      "EXP_RECEIVED_DATE": "2021-03-23T00:00:00+07:00",
+      "DETAIL": [
+        {
+          "SKU_CODE": "PILLOWB001",
+          "RECEIVED_QTY": "10"
+        }
+      ]
+    });
+
+
+    // Example Advance Shipment Notice
+    const createASN = await client.ASN.create({
+      "Rec1 No": "REC0000000001",
+      "EXP_RECEIVED_DATE": "2021-03-23T00:00:00+07:00",
+      "DETAIL": [
+        {
+          "SKU_CODE": "PILLOWB001",
+          "RECEIVED_QTY": "10"
+        }
+      ]
+    });
+
+    // Example Base
+    const createBase = await client.BASE.create({
+      "DATA": [
+        {
+          "BASE_TYPE": "COLORS",
+          "BASE_KEY": "RED",
+          "BASE_VALUE": "Red"
+        }
+      ]
+    });
+
+    const updateBase = await client.BASE.update({
+      {
+        "BASE_TYPE": "COLORS",
+        "FROM_BASE_KEY": "RED",
+        "FROM_BASE_VALUE": "Red",
+        "TO_BASE_KEY": "BLUE",
+        "TO_BASE_VALUE": "Blue"
+      }
+    });
+
+    const renameBaseType = await client.BASE.renameType({
+      {
+        "FROM_BASE_TYPE": "COLOR",
+        "TO_BASE_TYPE": "COLORS"
+      }
+    });
+
+
+    // Example Confirm Picked
+    const pickByJob = await client.Picked.selectByJob({
+      "Rel1 No": "QQ",
+    });
+
+
+    // Example Confirm Shipped
+    const shipByJob = await client.Shipped.selectByJob({
+      "Rel1 No": "QQ",
+    });
+
+
+    // Example Direct Shipment Order
+    const createDSO = await client.DSO.create({
+      "Rel1 No": "REC0000000002",
+      "PSO_DATE": "2021-03-23T00:00:00+07:00",
+      "DETAIL": [
+        {
+          "SKU_CODE": "PILLOWB001",
+          "RELEASE_QTY": "10"
+        }
+      ]
+    });
+
+    const updateConsigneeShipToDSO = await client.DSO.updateConsigneeShipTo({
+      "Rel1 No": "REC0000000002",
+      "CON_CODE": "ITH5",
+    });
+
+    const updateInvoiceDSO = await client.DSO.updateInvoice({
+      "Rel1 No": "REC0000000002",
+      "INVOICE_NO": "INV0000000001",
+    });
+
+    const cancelDSO = await client.DSO.cancel({
+      "Rel1 No": "REC0000000002",
+    });
+
+
+    // Example Goods Receive
+    const grByJob = await client.GR.selectByJob({
+      "Rec1 No": "QQ",
+    });
+
+
+    // Example Goods Receive Return
+    const grrByJob = await client.GRR.selectByJob({
+      "Rec1 No": "RT",
+    });
+
+
+    // Example Inventory
     const inventoryList = await client.Inventory.select({
       "PAGE": 1,
       "SIZE": 10,
@@ -30,39 +136,15 @@ async function main() {
     });
 
 
-    var pickByJob = await client.Picked.selectByJob({
-      "Rel1 No": "QQ",
+    // Example Inventory Movement
+    const invMovemenByJob = await client.InventoryMovement.selectByJob({
+      "MOVEMENT_ID": 72,
+      "TYPE": "HLD"
     });
 
 
-
-    var packByJob = await client.Packed.selectByJob({
-      "Rel1 No": "QQ",
-    });
-
-    var shipByJob = await client.Shipped.selectByJob({
-      "Rel1 No": "QQ",
-    });
-
-    var grByJob = await client.GR.selectByJob({
-      "Rec1 No": "QQ",
-    });
-
-
-
-    var crosscheckASN = await client.Crosscheck.selectASN({
-      "CREATE_DATE_FROM": "2021-03-23T00:00:00+07:00",
-      "CREATE_DATE_TO": "2021-03-23T23:59:59+07:00",
-    });
-
-    var crosscheckDSO = await client.Crosscheck.selectDSO({
-      "CREATE_DATE_FROM": "2021-03-23T00:00:00+07:00",
-      "CREATE_DATE_TO": "2021-03-23T23:59:59+07:00",
-    });
-
-
-
-    var createPartner = await client.Partner.createUpdate({
+    // Example Partner
+    const createPartner = await client.Partner.createUpdate({
       "DATA": [
         {
           "ACTIVE": true,
@@ -73,8 +155,8 @@ async function main() {
     });
 
 
-
-    var createSKU = await client.SKU.createUpdate({
+    // Example SKU
+    const createSKU = await client.SKU.createUpdate({
       "DATA": [
         {
           "SKU_CODE": "ABC0001",
@@ -107,7 +189,7 @@ async function main() {
       ]
     });
 
-    var updateBOM = await client.SKU.updateBOM({
+    const updateBOM = await client.SKU.updateBOM({
       "DATA": [
         {
           "SKU_CODE": "ABC0001",
@@ -119,59 +201,6 @@ async function main() {
           ]
         }
       ]
-    });
-
-
-
-    var createASN = await client.ASN.create({
-      "Rec1 No": "REC0000000001",
-      "EXP_RECEIVED_DATE": "2021-03-23T00:00:00+07:00",
-      "DETAIL": [
-        {
-          "SKU_CODE": "PILLOWB001",
-          "RECEIVED_QTY": "10"
-        }
-      ]
-    });
-
-
-
-    var createPSO = await client.PSO.create({
-      "Rel1 No": "REC0000000001",
-      "PSO_DATE": "2021-03-23T00:00:00+07:00",
-      "DETAIL": [
-        {
-          "SKU_CODE": "PILLOWB001",
-          "RELEASE_QTY": "10"
-        }
-      ]
-    });
-
-
-
-    var createDSO = await client.DSO.create({
-      "Rel1 No": "REC0000000002",
-      "PSO_DATE": "2021-03-23T00:00:00+07:00",
-      "DETAIL": [
-        {
-          "SKU_CODE": "PILLOWB001",
-          "RELEASE_QTY": "10"
-        }
-      ]
-    });
-
-    var updateConsigneeShipToDSO = await client.DSO.updateConsigneeShipTo({
-      "Rel1 No": "REC0000000002",
-      "CON_CODE": "ITH5",
-    });
-
-    var updateInvoiceDSO = await client.DSO.updateInvoice({
-      "Rel1 No": "REC0000000002",
-      "INVOICE_NO": "INV0000000001",
-    });
-
-    var cancelDSO = await client.DSO.cancel({
-      "Rel1 No": "REC0000000002",
     });
     */
 
